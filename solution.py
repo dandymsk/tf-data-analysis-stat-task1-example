423cea6a0122c45623283a42692bc3a4730d3a92
dandymsk <96170484+dandymsk@users.noreply.github.com>
2023-04-03 12:08:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 841977 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
+    n = len(x)
+    lambda_hat = x.sum() / (n * 46)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return lambda_hat 
